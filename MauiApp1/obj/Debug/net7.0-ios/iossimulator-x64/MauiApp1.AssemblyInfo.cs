7bd1e2ce7a98d77fb8bd5ace3bab12f90dfa5679
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2024-10-23 20:15:11 +0300

    test 6

--- MauiApp1/obj/Debug/net7.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net7.0-ios/iossimulator-x64/MauiApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d61c16185cc876545984cb9ad1813d6abd95d87c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
