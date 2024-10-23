d5299c3be0630b5a12e4a29a9bdcce66ff1f2dbb
RaneemIbra <132738285+RaneemIbra@users.noreply.github.com>
2024-10-23 19:49:32 +0300

    hello flux

---
+++ MauiApp1/page2.xaml.cs
@@ -0,0 +1,19 @@
+namespace MauiApp1;
+
+public partial class page2 : ContentPage
+{
+	private int clicked = 0;
+	public page2()
+	{
+		InitializeComponent();
+	}
+
+	private void Button2_OnClicked(object sender, EventArgs e)
+	{
+		clicked++;
+		if (clicked == 3)
+		{
+			label1.IsVisible = true;
+		}
+	}
+}
